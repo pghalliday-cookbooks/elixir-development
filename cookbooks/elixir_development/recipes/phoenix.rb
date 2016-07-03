4f4212f02de25925de104453e24361df3aedaf18
Peter Halliday <pghalliday@gmail.com>
2016-07-03 17:45:30 +0200

    nodejs

--- cookbooks/elixir_development/recipes/phoenix.rb
+++ cookbooks/elixir_development/recipes/phoenix.rb
@@ -10,3 +10,20 @@ bash 'install_phoenix' do
   user login_user
   not_if "su - #{login_user} -c 'mix phoenix.new -v'"
 end
+
+%w{
+apt-transport-https
+build-essential
+}.each do |name|
+  package name
+end
+
+apt_repository 'nodejs' do
+  uri 'https://deb.nodesource.com/node_6.x'
+  distribution node['lsb']['codename']
+  components ['main']
+  key 'https://deb.nodesource.com/gpgkey/nodesource.gpg.key'
+  deb_src true
+end
+
+package 'nodejs'
